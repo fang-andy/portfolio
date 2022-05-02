35f73b3e56694128254f845332ae0736d86a1149
Andy Fang <afang1094@gmail.com>
2022-05-02 13:24:57 -0700

    created logo and navbar files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "433e211b08ddabd6"; }
+/******/ 		__webpack_require__.h = function() { return "f5ddfc99f47d5a2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
