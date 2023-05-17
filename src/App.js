d5989b706d2c146015eb5ef9f669629f2445f43f
fang-andy <afang1094@gmail.com>
2023-05-17 10:50:37 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+    About,
+    Banner,
+    Contact,
+    Header,
+    Nav,
+    Services,
+    Work
+} from './components';
+
+const App = () => {
+    return (
+        <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+            <Header />
+            <Banner />
+            <Nav />
+            <About />
+            <Services />
+            <Work />
+            <Contact />
+        </div>
+    )
+}
